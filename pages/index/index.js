90a5c1875cbecc2ad1f6610a6530c30d2d32b8b7
zhaojianfei <zhaojianfei@aoji.cn>
2018-12-07 17:27:39 +0800

    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,8 +32,6 @@ Page({
     e.currentTarget;
     e.currentTarget.dataset.customdata;
 
-
-
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
@@ -55,33 +53,7 @@ Page({
   },
   // 生命周期回调—监听页面加载
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
     
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
   // 页面显示/切入前台时触发。
   onShow:function(e){
